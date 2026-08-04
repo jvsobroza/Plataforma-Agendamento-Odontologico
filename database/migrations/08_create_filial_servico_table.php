ae39d1a4896538dbb5aad71476ca328f7986d116
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2026-08-04 14:59:45 -0300

    refactor(database): Renomeação de atríbuto na migration de filial_servico

--- database/migrations/08_create_filial_servico_table.php
+++ database/migrations/08_create_filial_servico_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('filial_servico', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_filial')->constrained('filiais')->onDelete('cascade');
-            $table->foreignId('servico_id')->constrained('servicos')->onDelete('cascade');
+            $table->foreignId('id_servico')->constrained('servicos')->onDelete('cascade');
             $table->timestamps();
         });
     }
