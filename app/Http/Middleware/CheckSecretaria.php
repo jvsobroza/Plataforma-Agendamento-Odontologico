6d792edd7242ccd094cd84c523b883c8c2e01a3b
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2026-08-07 14:50:32 -0300

    feat(middleware): Criação dos middleware para tipo de usuário

---
+++ app/Http/Middleware/CheckSecretaria.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckSecretaria
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user()) {
+            return redirect()->route('login');
+        }
+
+        if ($request->user()->tipo != 2) {
+            abort(403, 'Acesso não autorizado.');
+        }
+
+        return $next($request);
+    }
+}
