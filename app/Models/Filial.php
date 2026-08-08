d5d89b7c5892efb72ff00e68f39b9f83b4c35f8d
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2026-08-08 14:30:52 -0300

    fix(models): Erro no nome da tabela em filial corrigido

--- app/Models/Filial.php
+++ app/Models/Filial.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Filial extends Model
 {
-    protected $table = 'filiais';
+    protected $table = 'filials';
     protected $fillable = [
         'cidade',
         'endereco',
