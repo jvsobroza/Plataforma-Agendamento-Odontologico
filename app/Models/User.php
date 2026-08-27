8ec3fa889d9609673d921bc3b50b1014a9621e03
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2026-08-27 19:47:57 -0300

    fix(model): Erro na model do usuário, não estava criptografando a senha

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'password' => 'hashed',
+            'senha' => 'hashed',
         ];
     }
     protected $table = 'usuarios';
