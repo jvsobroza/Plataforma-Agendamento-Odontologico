cadddbb8886872b76b955ae9ff0ca35285e71f1a
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2026-08-30 16:11:42 -0300

    style(sidebar): Adicionado ao clicar em uma rota na sidebar ela trocar de cor

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside class="sidebar">
-
+  
   <div class="sidebar-brand">
     <div class="sidebar-brand-badge">
       <img src="{{ asset('images/logo.png') }}" alt="Sante Oral Odontologia">
@@ -11,44 +11,44 @@
   <ul class="sidebar-nav">
     <li>
       @if (auth()->user()->tipo == 1)
-      <a href="{{ route('dentista.index') }}" class="sidebar-link">
+      <a href="{{ route('dentista.index') }}" class="sidebar-link {{ request()->routeIs('dentista.index') ? 'active' : '' }}">
         <i class="bi bi-grid-fill"></i>
         <span>Dashboard</span>
       </a>
       @else
-      <a href="{{ route('secretaria.index') }}" class="sidebar-link">
+      <a href="{{ route('secretaria.index') }}" class="sidebar-link {{ request()->routeIs('secretaria.index') ? 'active' : '' }}">
         <i class="bi bi-grid-fill"></i>
         <span>Dashboard</span>
       </a>
       @endif
     </li>
     <li>
-      <a href="{{ route('agendamentos.index') }}" class="sidebar-link">
+      <a href="{{ route('agendamentos.index') }}" class="sidebar-link {{ request()->routeIs('agendamentos.*') ? 'active' : '' }}">
         <i class="bi bi-calendar3"></i>
         <span>Agenda</span>
       </a>
     </li>
     <li>
-      <a href="{{ route('pacientes.index') }}" class="sidebar-link">
+      <a href="{{ route('pacientes.index') }}" class="sidebar-link {{ request()->routeIs('pacientes.*') ? 'active' : '' }}">
         <i class="bi bi-person"></i>
         <span>Pacientes</span>
       </a>
     </li>
     @if (auth()->user()->tipo == 1)
     <li>
-      <a href="{{ route('dentista.servicos.index') }}" class="sidebar-link">
+      <a href="{{ route('dentista.servicos.index') }}" class="sidebar-link {{ request()->routeIs('dentista.servicos.*') ? 'active' : '' }}">
         <i class="bi bi-currency-dollar"></i>
         <span>Serviços</span>
       </a>
     </li>
     <li>
-      <a href="{{ route('dentista.filiais.index') }}" class="sidebar-link">
+      <a href="{{ route('dentista.filiais.index') }}" class="sidebar-link {{ request()->routeIs('dentista.filiais.*') ? 'active' : '' }}">
         <i class="bi bi-shop"></i>
         <span>Filiais</span>
       </a>
     </li>
     <li>
-      <a href="{{ route('dentista.secretarias.index') }}" class="sidebar-link">
+      <a href="{{ route('dentista.secretarias.index') }}" class="sidebar-link {{ request()->routeIs('dentista.secretarias.*') ? 'active' : '' }}">
         <i class="bi bi-person-vcard"></i>
         <span>Secretárias</span>
       </a>
