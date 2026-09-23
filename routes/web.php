4ff03bd14d94c94baa8cbaf32d43afea2409ad04
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2026-09-23 16:42:09 -0300

    fix(route/session): Arrumado a questão de sair do navegador e continuar logado, agora mandando pro local certo de login

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/login', function () {
-    return view('login');
+    return view('welcome');
 })->name('login');
 
 Route::post('/login', function (Request $request) {
